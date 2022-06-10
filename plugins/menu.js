c03643b8737700184f65673c26eade4e5f51c6b7
AlmightySvd <106898457+AlmightySvd@users.noreply.github.com>
2022-06-09 20:10:10 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -23,7 +23,7 @@ let tags = {
 }
 const defaultMenu = {
   before: `
-  ────  *DyLux  ┃ ᴮᴼᵀ*  ────
+  ────  *𝙇𝙚𝙜𝙖𝙡𝙞𝙯𝙖𝙡𝙖 𝙥𝙚 𝙘𝙖𝙪𝙨𝙖🍁  ┃ ᴮᴼᵀ*  ────
 
 👋🏻 _Hola_ *%name*
 
@@ -148,7 +148,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   //const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
 const pp = await (await fetch('https://i.ibb.co/qMG1JPY/fg.jpg')).buffer()
     
-    conn.sendHydrated(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n', pp, 'https://youtube.com/fg98f', 'YouTube', null, null, [
+    conn.sendHydrated(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/mfer_mf10\n', pp, 'https://www.instagram.com/mfer_mf10', 'YouTube', null, null, [
       ['ꨄ︎ Apoyar', '/donate'],
       ['⏍ Info', '/botinfo'],
       ['✆ Owner', '/owner']
